662d7e71b28b67e164b26de6e169aae173593177
Ksenia de Leo <deleokse@max-cmsg001.desy.de>
2019-10-14 13:49:26 +0200

    modify copy_inputs.sh

--- input/copy_inputs.sh
+++ input/copy_inputs.sh
@@ -1,3 +1,5 @@
+scp -r deleokse@naf-cms.desy.de:/nfs/dust/cms/user/deleokse/ZprimeSemiLeptonic/94X_v1/MLtests_numpy/2017_Moriond19JEC_RightLumiweights_forml_morevar_Puppi/MLInput .
+
 #scp -r karavdia@naf-cms12.desy.de:/nfs/dust/cms/user/reimersa/ZprimeSemiLeptonic/94X_v1/Fullselection/2017_Moriond19JEC_RightLumiweights_forml_Puppi/NOMINAL/MLInput .
 
 #scp -r karavdia@naf-cms12.desy.de:/beegfs/desy/user/reimersa/ZprimeClassifier/input/classes_4_QCD_Mu_TTbar+ST_DYJets+WJets_RSGluon_All/ .
